00ce3d6d9b16cccd03475f4461e260a6f987b218
Scott Young <scott.young@getbridge.com>
2023-02-23 12:49:20 -0700

    update tailwind config to include components folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,tsx}"],
+  content: [
+    './src/**/*.{html,js,jsx,tsx}',
+    './src/pages/components/**/*.{html,js,jsx,tsx}',
+  ],
   theme: {
     extend: {
       backgroundImage: {
-        'hero-image': "linear-gradient(87deg, rgba(2,0,36,0.8) 0%, rgba(23,23,40,0.8) 35%, rgba(20,56,64,0.8) 78%), url('../images/mountains-background.jpg')"
+        'hero-image':
+          "linear-gradient(87deg, rgba(2,0,36,0.8) 0%, rgba(23,23,40,0.8) 35%, rgba(20,56,64,0.8) 78%), url('../images/mountains-background.jpg')",
       },
       backgroundSize: {
-        'hero-image': "cover"
-      }
+        'hero-image': 'cover',
+      },
     },
   },
   plugins: [],
