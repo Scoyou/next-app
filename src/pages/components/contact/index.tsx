50e2edab1aee37753cac73d56db4239eb7607d6a
Scott Young <scott.young@getbridge.com>
2023-02-23 12:25:08 -0700

    lorem ipsum filler

--- src/pages/components/contact/index.tsx
+++ src/pages/components/contact/index.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React from 'react'
 
 const Contact = () => {
   return (
     <div id="contact" className="h-screen grid place-items-center">
-    <h2 className="text-center mb-4">Contact</h2>
-    <div className="grid grid-cols-1 place-items-center md:w-screen">
-      <div>one</div>
-      <div>two</div>
-      <div>three</div>
+      <h2 className="text-center mb-4">Contact</h2>
+      <div className="grid grid-cols-1 place-items-center md:w-screen">
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
     </div>
-  </div>
   )
 }
 
--- src/pages/components/projects-carousel/index.tsx
+++ src/pages/components/projects-carousel/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 
 const ProjectsCarousel = () => {
+  const renderProject = () => (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </div>
+  )
   return (
     <div id="projects" className="h-screen grid place-items-center">
       <h2 className="text-center mt-6 mb-6">Projects</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 place-items-center md:w-screen">
-        <div>one</div>
-        <div>two</div>
-        <div>three</div>
+        {renderProject()}
+        {renderProject()}
+        {renderProject()}
       </div>
     </div>
   )
