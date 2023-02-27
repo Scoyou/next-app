0724f0564ff96ae3663837f229f2f26928a3d817
Scott Young <scott.young@getbridge.com>
2023-02-27 14:33:24 -0700

    prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,56 @@
-import React, { useRef, useState, createContext, useEffect } from 'react'
-import HomePageCard from './components/home-page-card'
-import Navbar from './components/navbar'
-import ProjectsCarousel from './components/projects-carousel'
-import Contact from './components/contact'
+import React, { useRef, useState, createContext, useEffect } from "react";
+import HomePageCard from "./components/home-page-card";
+import Navbar from "./components/navbar";
+import ProjectsCarousel from "./components/projects-carousel";
+import Contact from "./components/contact";
 import Image from "next/legacy/image";
-import darkHero from '../images/night-mountains.jpg'
-import lightHero from '../images/day-mountains.jpg'
+import darkHero from "../images/night-mountains.jpg";
+import lightHero from "../images/day-mountains.jpg";
 
-export const DarkThemeContext = createContext({})
+export const DarkThemeContext = createContext({});
 
 const Home = () => {
-  const home = useRef<HTMLDivElement>(null)
-  const projects = useRef<HTMLDivElement>(null)
-  const contact = useRef<HTMLDivElement>(null)
-  const [darkModeEnabled, setDarkModeEnabled] = useState<boolean>(true)
+  const home = useRef<HTMLDivElement>(null);
+  const projects = useRef<HTMLDivElement>(null);
+  const contact = useRef<HTMLDivElement>(null);
+  const [darkModeEnabled, setDarkModeEnabled] = useState<boolean>(true);
 
   useEffect(() => {
-    const storedTheme = localStorage.getItem('theme')
-    setDarkModeEnabled(storedTheme === 'dark')
-  }, [darkModeEnabled])
+    const storedTheme = localStorage.getItem("theme");
+    setDarkModeEnabled(storedTheme === "dark");
+  }, [darkModeEnabled]);
 
   const navHeader = [
     {
-      headerTitle: 'Home',
+      headerTitle: "Home",
       headerRef: home,
-      headerID: 'home',
+      headerID: "home",
     },
     {
-      headerTitle: 'Projects',
+      headerTitle: "Projects",
       headerRef: projects,
-      headerID: 'projects',
+      headerID: "projects",
     },
     {
-      headerTitle: 'Contact',
+      headerTitle: "Contact",
       headerRef: contact,
-      headerID: 'contact',
+      headerID: "contact",
     },
-  ]
+  ];
 
   const changeTheme = () => {
-    const theme = darkModeEnabled ? 'light' : 'dark'
-    localStorage.setItem('theme', theme)
-    setDarkModeEnabled(!darkModeEnabled)
-  }
+    const theme = darkModeEnabled ? "light" : "dark";
+    localStorage.setItem("theme", theme);
+    setDarkModeEnabled(!darkModeEnabled);
+  };
 
   return (
     <DarkThemeContext.Provider value={{ changeTheme, darkModeEnabled }}>
       <div
         className={
           darkModeEnabled
-            ? 'dark grid place-items-center text-white'
-            : 'text-black grid place-items-center'
+            ? "dark grid place-items-center text-white"
+            : "text-black grid place-items-center"
         }
       >
         <Navbar navHeader={navHeader} />
@@ -76,7 +76,7 @@ const Home = () => {
         </div>
       </div>
     </DarkThemeContext.Provider>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
