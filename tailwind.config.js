7c6802320882a13d39a4d19210cf49ff4ebc4545
Scott Young <scott.young@getbridge.com>
2023-02-27 14:02:20 -0700

    prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
       backgroundImage: {
         "hero-image-gradient":
           "linear-gradient(87deg, rgba(2,0,36,0.8) 0%, rgba(23,23,40,0.8) 35%, rgba(20,56,64,0.8) 78%)",
-        "projects-background": "linear-gradient(87deg,rgba(2, 0, 36, 1) 0%,rgba(23, 23, 40, 1) 35%,rgba(20, 56, 64, 1) 78%)"
+        "projects-background":
+          "linear-gradient(87deg,rgba(2, 0, 36, 1) 0%,rgba(23, 23, 40, 1) 35%,rgba(20, 56, 64, 1) 78%)",
       },
       backgroundSize: {
         "hero-image-gradient": "cover",
